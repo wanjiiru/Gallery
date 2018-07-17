52e25d6a074b15630a131a355f34282746d7ed73
wanjiiru <liz240@live.com>
2018-07-17 21:33:20 +0300

    redesign navbar

--- collection/tests.py
+++ collection/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(name='test',pic='pic.jpg',description='tets4' ,image_location ='Location',image_category='Shoes')
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance((self.image,Image)))
