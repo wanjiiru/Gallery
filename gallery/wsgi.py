c7b105d88beabc5203e626ce61a3e20a071fc088
wanjiiru <liz240@live.com>
2018-07-19 11:54:26 +0300

    stuff

--- gallery/wsgi.py
+++ gallery/wsgi.py
@@ -16,5 +16,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings")
 
 
 application = get_wsgi_application()
-application  = DjangoWhiteNoise(application)
+application=DjangoWhiteNoise(application)
 
